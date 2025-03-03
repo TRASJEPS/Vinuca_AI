fddb36354ffeddf2a3edb8e512b1b3a3a95efb23
Travis Jepson <71234985+TRASJEPS@users.noreply.github.com>
2025-03-02 22:53:22 -0800

    Playing with components

--- client/vinuca_ai/src/app/signup/signup.tsx
+++ client/vinuca_ai/src/app/signup/signup.tsx
@@ -1,4 +1,23 @@
-export default function Signup() {
-    console.log("Sign up component");
-    return <h1>Sign Up</h1>
+export const Signup = () => {
+    console.log("Sign Up Component");
+    
+    const signUpText = 
+{
+    margin: "5px",
+    marginTop: "15px",
+    marginLeft: "20px",
+    marginRight: "20px",
+    paddingTop: "10px",
+    paddingBottom: "10px",
+    paddingLeft: "20px",
+    paddingRight: "20px",
+    background: "rgb(0, 148, 203)",
+    color: "white",
+    fontWeight: "bolder",
+    fontSize: "large",
+    border: "none",
+    borderRadius: "15px",
+};
+    
+    return <div style={signUpText}>Sign Up Banner</div>
 }
