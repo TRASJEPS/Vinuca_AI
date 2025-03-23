1ee79c031821ca0fcd931850eeb41138c1f8dbf2
Travis Jepson <71234985+TRASJEPS@users.noreply.github.com>
2025-03-22 22:20:17 -0700

    spinner and react fragment
    to get spinner working, still editing some other parts

--- client/vinuca_ai/src/app/components/chatbot.tsx
+++ client/vinuca_ai/src/app/components/chatbot.tsx
@@ -47,13 +47,25 @@ export default function Chat() {
       ></textarea>
 
       {/* THE SEND BUTTON / CSS COLORS */}
+      {/* PINK to BLUE */}
       <button
         className={`mt-2 px-4 py-2 ${loading ? 'bg-[rgb(0,0,255)]' : 'bg-[rgb(255,149,202)]'} text-white font-bold rounded disabled:opacity-50`}
         onClick={sendMessage}
         disabled={loading}
       >
         {/* THE SEND BUTTON / LOADING  */}
-        {loading ? ( "Let me think..." ) : "Send"}
+        {/* The SVG needs a react 'react fragment' wrapper <></> */}
+        {loading ? ( 
+          <>
+          <svg className="mr-3 size-5 animate-spin ..." viewBox="0 0 24 24">
+            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+            <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+          </svg>
+          Let me think... 
+          </>
+          ) 
+          
+        : "Send"}
       </button>
 
       {response && (
